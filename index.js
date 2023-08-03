f0187632e359219fe4871c970e04728f80e3d7b6
ilseuwu <141301519+ilseuwu@users.noreply.github.com>
2023-08-02 23:36:59 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Te conoci cuando menos lo espera y sin darme cuenta te convertiste en una persona muy especial y valiosa para mi,estoy feliz de haberte conocido, ademas quiero que tengas en cuenta que te quiero mas de lo que he podido demostrar,eres mi persona favorita en todo el mundo y solo queria hacertelo saber')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click',function() {
+    alert('te quiero mucho mi niño bonito pero es el otro boton')
+});
